81e29666e0e2269dce2c1df64a9afa2302094384
Yvain Hoekstra <konirosen@protonmail.ch>
2018-10-30 15:46:44 -0300

    Update index.py
    Removed os import and limited the sys import.

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
-import sys
-from os import remove
+from sys import exit
 from lib.fileGenerate import initFile
 from lib.JSONCheck import JSONCheck
 import json #For initial reading of the JSON file
@@ -12,14 +11,14 @@ if (fileLocation.startswith("C:") != True): #Easier if it's in root folder
 try:
     open(fileLocation, "r")
 except FileNotFoundError:
-    sys.exit("Error: " + fileLocation + " is not found.") #File not found
+    exit("Error: " + fileLocation + " is not found.") #File not found
 
 data = json.loads(open(fileLocation).read()) #JSON data
-AESLPATH = fileLocation[0:-4] + "aesl" #AESL file path
 
 result = JSONCheck(data) #Checks for missing JSON values.
 if (result == True): #If true, exit program. Error message is printed already.
-    sys.exit()
+    exit()
 
+AESLPATH = fileLocation[0:-4] + "aesl" #AESL file path
 convertFile = initFile(open(AESLPATH, "w")) #Inits the AESL file
 convertFile.setUp(data, AESLPATH) #Translate AESL file
